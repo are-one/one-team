1196683be6b011ea71f88353403d3114fd5209d5
Are1PM <arwanpriantomangidi@gmail.com>
2021-12-12 01:39:57 +0800

    [L1] import data dari file js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// console.log(global);
+const importedStuff = require('./users');
 
-global.setTimeout(() => {
-    console.log("Set Time Out with global");
-}, 3000);
-
-setTimeout(() => {
-    console.log('Set Time Out without global');
-}, 3000);
-
-console.log(__dirname);
-console.log(__filename);
+console.log(importedStuff);
